3c14f3c597dfb0171e0db5414c0e86da8081d047
rodgeur (Regis Ruelland) <regis.ruelland@laas.fr>
2024-05-23 17:18:52 +0200

    renamed factory.[hc] to control_factory.[hc]

---
+++ src/control_factory.cpp
@@ -0,0 +1,47 @@
+#include "control_factory.h"
+
+Pid ControlFactory::pid(float32_t Ts, float32_t Kp, float32_t Ti, float32_t Td, float32_t N, float32_t lower_bound, float32_t upper_bound) {
+    PidParams p(Ts, Kp, Ti, Td, N, lower_bound, upper_bound);
+    Pid controller = Pid();
+    controller.init(p);
+    return controller;
+}
+
+Pr ControlFactory::pr(float32_t Ts, float32_t Kp, float32_t Kr, float32_t w0, float32_t phi_prime, float32_t lower_bound, float32_t upper_bound){
+    PrParams p(Ts, Kp, Kr, w0, phi_prime, lower_bound, upper_bound);
+    Pr controller = Pr();
+    controller.init(p);
+    return controller;
+}
+
+RST ControlFactory::rst(float32_t Ts, uint8_t nr, const float32_t *r, uint8_t ns, const float32_t *s, uint8_t nt, const float32_t *t, float32_t lower_bound, float32_t upper_bound){
+    RstParams p(Ts, nr, r, ns, s, nt, t, lower_bound, upper_bound);
+    RST controller = RST();
+    controller.init(p);
+    return controller;
+}
+
+PllSinus ControlFactory::pllSinus(float32_t Ts, float32_t amplitude, float32_t f0, float32_t rise_time){
+    PllSinus pll = PllSinus(Ts, amplitude, f0, rise_time);
+    return pll;
+}
+
+PllAngle ControlFactory::pllAngle(float32_t Ts, float32_t f0, float32_t rise_time) {
+    PllAngle pll = PllAngle(Ts, f0, rise_time);
+    return pll;
+}
+
+NotchFilter ControlFactory::notchfilter(float32_t Ts, float32_t f0, float32_t bandwidth){
+    NotchFilter filter = NotchFilter(Ts, f0, bandwidth);
+    filter.reset();
+    return filter;
+}
+
+LowPassFirstOrderFilter  ControlFactory::lowpassfilter(float32_t Ts, float32_t tau){
+    LowPassFirstOrderFilter filter = LowPassFirstOrderFilter(Ts, tau);
+    filter.reset();
+    return filter;
+}
+
+ControlFactory controlLibFactory = ControlFactory();
+
---
+++ src/control_factory.h
@@ -0,0 +1,90 @@
+#include "pid.h"
+#include "pr.h"
+#include "rst.h"
+#include "filters.h"
+
+
+class ControlFactory {
+    public:
+    ControlFactory() {};
+    /**
+     * @brief return a Pid controller in a standard form.
+     * 
+     * @param Ts sample time[s]
+     * @param Kp proportional gain
+     * @param Ti integral time [s]
+     * @param Td derivative time [s] 
+     * @param N to define derivative constant time filter 
+     * @param lower_bound 
+     * @param upper_bound 
+     * @return Pid 
+     */
+    Pid pid(float32_t Ts, float32_t Kp, float32_t Ti, float32_t Td, float32_t N, float32_t lower_bound, float32_t upper_bound);
+    /**
+     * @brief return a Proportional Resonant controller for a fixed pulsation `w0`[rad/s] and possible advanced phase `phi_prime`[rad]
+     * 
+     * @param Ts sampling time [s] 
+     * @param Kp proportional gain 
+     * @param Kr resonant gain
+     * @param w0 pulsation [rad/s]
+     * @param phi_prime advanced phase [rad]
+     * @param lower_bound 
+     * @param upper_bound 
+     * @return * Pr 
+     */
+    Pr pr(float32_t Ts, float32_t Kp, float32_t Kr, float32_t w0, float32_t phi_prime, float32_t lower_bound, float32_t upper_bound);
+    /**
+     * @brief return a polynomial RST controller
+     * 
+     * @param Ts sampling time [s]
+     * @param nr number of R coefs 
+     * @param *r  pointer to array R
+     * @param ns number of S coefs
+     * @param *s pointer to array S
+     * @param nt number of T coefs
+     * @param *t pointer to array T
+     * @param lower_bound 
+     * @param upper_bound 
+     * @return RST 
+     */
+    RST rst(float32_t Ts, uint8_t nr, const float32_t *r, uint8_t ns, const float32_t *s, uint8_t nt, const float32_t *t, float32_t lower_bound, float32_t upper_bound);
+    /**
+     * @brief return a phase locked loop filter adapated to sinus tracking at a fixed `f0`[Hz] frequency with a fixed `amplitude` and with a `rise_time` [s] dynamic
+     * 
+     * @param Ts sampling time [s]
+     * @param amplitude 
+     * @param f0  frequency [Hz]
+     * @param rise_time [s]
+     * @return PllSinus 
+     */
+    PllSinus pllSinus(float32_t Ts, float32_t amplitude, float32_t f0, float32_t rise_time);
+    /**
+     * @brief return a phase locked loop filter adapated to a saw-tooth between [0, 2Π], tracking at a fixed `f0`[Hz] frequency with a `rise_time` [s] dynamic
+     * 
+     * @param Ts sampling time [s]
+     * @param f0  frequency [Hz]
+     * @param rise_time [s]
+     * @return PllAngle
+     */
+    PllAngle pllAngle(float32_t Ts, float32_t f0, float32_t rise_time);
+
+    /**
+     * @brief return a notch filter around the frequency `f0`[Hz] with a `bandwidth` [Hz]
+     * 
+     * @param Ts sampling time [s]
+     * @param f0 frequency [Hz]
+     * @param bandwidth bandwidth around f0 where gain < -3dB [Hz]
+     * @return NotchFilter 
+     */
+    NotchFilter notchfilter(float32_t Ts, float32_t f0, float32_t bandwidth);
+    /**
+     * @brief low pass filter
+     * 
+     * @param Ts sampling time [s]
+     * @param tau constant time [s]
+     * @return LowPassFirstOrderFilter 
+     */
+    LowPassFirstOrderFilter lowpassfilter(float32_t Ts, float32_t tau);
+};
+
+extern ControlFactory controlLibFactory;
